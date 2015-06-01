f46c7e656005408b747186097ed50cfd2df6f740
Damir Mustafin <frenchthebread@gmail.com>
2015-06-01 16:39:50 +0300

    lists added

--- testapp.js
+++ testapp.js
@@ -1,9 +1,13 @@
 Tasks = new Mongo.Collection("tasks");
+Lists = new Mongo.Collection("lists");
 
 if (Meteor.isClient) {
     Template.body.helpers({
         tasks: function () {
             return Tasks.find({}, {sort: {createdAt: -1}});
+        },
+        lists: function () {
+            return Lists.find({}, {sort: {createdAt: -1}});
         }
     });
 
@@ -14,6 +18,21 @@ if (Meteor.isClient) {
 
             Tasks.insert({
                 text: text,
+                createdAt: new Date()
+            });
+
+            event.target.text.value = "";
+
+            return false;
+        },
+        "click .delete1": function () {
+            Lists.remove(this._id);
+        },
+        "submit .new-list": function (event) {
+            var list = event.target.text.value;
+
+            Lists.insert({
+                text: list,
                 createdAt: new Date()
             });
 
